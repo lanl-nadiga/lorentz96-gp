43cc4dc18d44e98ea38c06fe38eca3afc6015925
balu <btnadiga@gmail.com>
2013-01-29 11:18:28 -0700

    New functions sample2, like_pair, like_pair_1map, and unlike_pair.
    1. One of the issues is that the argument to Pool.map has to be picklable.
    1a. So, put funtions like_pair, like_pair_1map, and unlike_pair in gpsm.py
    1b. Noting that gpsm.py does not import the multiprocessing module.
    1c. If these functions were in improv_p2l2h.py Python throws errors about not being able to picle functions
    
    2. Put sample2 here so that I do not have to use sampler.py module
    
    3. Modified GPsubmodel in gp_model to have a specified variance rather than have that be a random variable. With this modification, iterations argument to sample actually reflects the number of iterations (unlike if the variance was a random variable when the number of iterations kept changing and I was not able to see the connection between specified iterations and the actual number of iterations specified.
    
    4. I can see possible advantages to having observation variance be a random varialbe if it were to be tuned as well. But the question is as to whether this is tuned by the sampling procedure???

--- gpsm.py
+++ gpsm.py
@@ -1,92 +1,30 @@
 import pymc
 from pymc import gp as gp
-import numpy
+import numpy as np
 import stochastic
 import model
 import mesh_low
 import mesh_high
 
-# WHAT IS I???
 def mean(I, h, b, c, F):
-	return numpy.zeros(len(I))
-	# return numpy.ones(len(I))*(F-10)*5
+	return np.zeros(len(I))
+	# return np.ones(len(I))*(F-10)*5
 
-'''
-def gp_model(parameter_lattice_points, d1, output_high, output_index):
-#	nu = pymc.Normal('nu', 1.5, 1.5, value=1.5)
-#	phi = pymc.Lognormal('phi', mu=.4, tau=1, value=1)
-#	theta = pymc.Lognormal('theta', mu=.5, tau=1, value=1)
-	#C = gp.Covariance(eval_fun = gp.matern.euclidean, diff_degree = nu, amp = phi, scale = theta)
-	#M = gp.Mean(eval_fun = stochastic.final, h=model.coupling, b=model.amplitude, c=model.timescale, F=model.forcing)
+def gp_model(d1, output, diff_deg_val=None, amp_val=None, scale_val=None):
 
 	# Prior parameters of C
+	diff_deg = pymc.Uniform('diff_deg', 1., 3.)
+	amp = pymc.Lognormal('amp', mu=.4, tau=.1)#, value=1)
+	scale = pymc.Lognormal('scale', mu=.5, tau=1)#, value=1)
 
-	nu = pymc.Uniform('nu', 1., 3.)
-	phi = pymc.Lognormal('phi', mu=.4, tau=.1)#, value=1)
-	theta = pymc.Lognormal('theta', mu=.5, tau=1)#, value=1)
-	
-	# The covariance dtrm C is valued as a Covariance object.
+	if diff_deg_val is not None: diff_deg.value = diff_deg_val
+	if amp_val is not None: amp.value = amp_val
+	if scale_val is not None: scale.value = scale_val
 
-	@pymc.deterministic
-	def C(eval_fun = gp.matern.euclidean, diff_degree = nu, amp = phi, scale = theta):
-		return gp.NearlyFullRankCovariance(eval_fun, diff_degree = diff_degree, amp = amp, scale = scale)
-
-#	beta = numpy.empty(d1.shape[1], dtype=object)
-#	for i in range(d1.shape[1]):
-#		beta[i] = pymc.Normal('beta_{0}'.format(i), mu=1, tau=1)
-
-#	h_weight = pymc.Normal('h_weight', mu=1., tau=0.5, value=1.)
-#	b_weight = pymc.Normal('b_weight', mu=1., tau=0.5, value=1.)
-#	c_weight = pymc.Normal('c_weight', mu=1., tau=0.5, value=1.)
-#	F_weight = pymc.Normal('F_weight', mu=1., tau=0.5, value=1.)
-
-	# The covariance dtrm C is valued as a Covariance object.
-	#@pymc.deterministic
-	#def C(eval_fun = gp.matern.euclidean, diff_degree=nu, amp=phi, scale=theta):
-	#	return gp.NearlyFullRankCovariance(eval_fun, diff_degree=diff_degree, amp=amp, scale=scale)
-
-
-#		def square_difference(a, b):
-#			return a**2 + b**2 - 2*a*b
-#		return numpy.array([ sum( square_difference(numpy.array(p), numpy.array([h,b,c,F])) * numpy.array([h_weight, b_weight, c_weight, F_weight]) ) for p in P])
-
-# The mean M is valued as a Mean object.
-
-	@pymc.deterministic
-	def M(eval_fun = mean, h=model.coupling, b=model.amplitude, c=model.timescale, F=model.forcing):
-		return gp.Mean(eval_fun, h=h, b=b, c=c, F=F)
-
-	# C = gp.Covariance(eval_fun = gp.matern.euclidean, diff_degree = 1.4, amp = 1., scale = 1.)
-	
-	#C = gp.Covariance(eval_fun = gp.matern.euclidean, diff_degree = nu, amp = phi, scale = theta)
-	#M = gp.Mean(eval_fun = test_mean, h=model.coupling, b=model.amplitude, c=model.timescale, F=model.forcing, h_weight=h_weight, b_weight=b_weight, c_weight=c_weight, F_weight=F_weight)
-
-	# Observations for the map from mesh_low to mesh_high are located at d1[parameter_lattice_points])
-	
-	GP = gp.GPSubmodel('gpsm', M, C, d1[parameter_lattice_points])
-	#gp.observe(M, C, mesh_low.input.reshape(625, 4), mesh_low.distance(model.Y0), numpy.zeros(625))
-	#C.observe(mesh_low.input.reshape(625, 4), numpy.ones(625))
-	#M.observe(C, mesh_low.input.reshape(625, 4), mesh_low.distance(model.Y0))
-
-	# Observation variance
-
-	variance = pymc.Lognormal('variance', mu=-1, tau=1., value=0.0001)
-	data = pymc.Normal('data', mu=GP.f(d1[parameter_lattice_points]), tau = 1./variance, value=output_high[:, output_index], observed=True)
-
-	l = locals()
-	return { k: l[k] for k in ['parameter_lattice_points', 'output_index', 'nu', 'phi', 'theta', 'GP', 'variance', 'data'] }
-'''
-
-def gp_model(parameter_lattice_points, d1, output, output_index):
-
-	# Prior parameters of C
-	nu = pymc.Uniform('nu', 1., 3.)
-	phi = pymc.Lognormal('phi', mu=.4, tau=.1)#, value=1)
-	theta = pymc.Lognormal('theta', mu=.5, tau=1)#, value=1)
 	
 	# The covariance dtrm C is valued as a Covariance object.
 	@pymc.deterministic
-	def C(eval_fun = gp.matern.euclidean, diff_degree = nu, amp = phi, scale = theta):
+	def C(eval_fun = gp.matern.euclidean, diff_degree = diff_deg, amp = amp, scale = scale):
 		return gp.NearlyFullRankCovariance(eval_fun,
 						   diff_degree = diff_degree, amp = amp, scale = scale)
 	# The mean M is valued as a Mean object.
@@ -95,34 +33,122 @@ def gp_model(parameter_lattice_points, d1, output, output_index):
 		return gp.Mean(eval_fun, h=h, b=b, c=c, F=F)
 
 	# Define, on appropriate mesh, the GP with mean M and covariance C
-	GP = gp.GPSubmodel('gpsm', M, C, d1[parameter_lattice_points])
-
-	# Observation variance
-	variance = pymc.Lognormal('variance', mu=-1, tau=1., value=0.0001)
+	GP = gp.GPSubmodel('gpsm', M, C, d1)
 
 	# Observations for the map to learn
-	data = pymc.Normal('data', mu=GP.f(d1[parameter_lattice_points]),
-			   tau = 1./variance, value=output[:, output_index], observed=True)
+	data = pymc.Normal('data', mu=GP.f(d1),
+			   tau = 1e4, value=output, observed=True)
+	
+	return locals()
 
-	l = locals()
-	return { k: l[k] for k in ['parameter_lattice_points', 'output_index',
-				   'nu', 'phi', 'theta', 'GP', 'variance', 'data'] }
+def sample2(tr_in, tr_out,hypers):
+	mc = pymc.MCMC(gp_model(tr_in, tr_out))#,*hypers))
+	mc.sample(100, burn = 500, thin=1)
+
+	return mc
+
+def like_pair_1map(input):
+
+	pair = input[0]
+	p2h_tr_full = input[1][0]
+
+	try:
+		hypers = input[1][1:]
+	except IndexError:
+		hypers = [None]*3
+		
+	#Make copy and move pair to end
+	tr_set = p2h_tr_full.copy()
+	tr_set[[-1,pair[0]]] = tr_set[[pair[0],-1]]
+	tr_set[[-2,pair[1]]] = tr_set[[pair[1],-2]]
+
+	mc2 = sample2(tr_set[:-2,:-1], tr_set[:-2,-1], hypers) #leave out last 2 points
+	ntr = len(mc2.GP.f.trace())
+	est2 = np.array([mc2.GP.f.trace()[itr]([tr_set[-1,:-1]])   #value at heldout pt. without extra point
+		      for itr in range(ntr)]).mean()
+
+	mc1 = sample2(tr_set[:-1,:-1], tr_set[:-1,-1], hypers) #leave out only heldout pt
+	est1 = np.array([mc1.GP.f.trace()[itr]([tr_set[-1,:-1]])   #value at heldout pt. with extra point
+		      for itr in range(ntr)]).mean()
+	
+	return abs(est2 - tr_set[-1,-1]) - abs(est1 - tr_set[-1,-1]) 
 
 
+def unlike_pair(input):
 
-# learn map from (params,low_pca_values) to high_pca_values
-gps = [gp_model(mesh_high.indices, mesh_low.d1, mesh_high.pca_output, i)
-       for i in range(mesh_high.PCA.n_components)]
+	pair = input[0]
+	p2l_full = input[1][0]
+	l2h_full = input[1][1]
 
-# learn map from params to high_pca_values
-gps_prm_to_high = [gp_model(mesh_high.indices, mesh_low.d1[:,:4], mesh_high.pca_output, i)
-		   for i in range(mesh_high.PCA.n_components)]
+	try:
+		hypers = input[1][2:]
+	except IndexError:
+		hypers = [None]*3
+		
+	#Make copy and move pair to end
+	p2l_set = p2l_full.copy()
+	p2l_set[[-1,pair[0]]] = p2l_set[[pair[0],-1]] #testpt.
+	p2l_set = np.delete(p2l_set, pair[1], axis=0) # delete holdout l2h point from p2l
 
-# learn map from params to low pca values
-gps_prm_to_low = [gp_model(mesh_low.indices, mesh_low.d1[:,:4], mesh_low.pca_output, i)
-		  for i in range(mesh_low.PCA.n_components)]
+	l2h_set = l2h_full.copy()
+	l2h_set[[-1,pair[1]]] = l2h_set[[pair[1],-1]] #holdout
 
-gps_low_to_high = [gp_model(mesh_high.indices, mesh_low.d1[:,4:], mesh_high.pca_output, i)
-		  for i in range(mesh_high.PCA.n_components)]
+	mc2 = sample2(p2l_set[:-1,:-1], p2l_set[:-1,-1], hypers) #leave out last point
+	ntr = len(mc2.GP.f.trace())
 
-# gps_fft = [ gp_model(mesh_low.d1_fft, mesh_high.pca_fft_output, i) for i in range(mesh_high.PCA_FFT.n_components) ]
+	tmp2 = np.array([mc2.GP.f.trace()[itr]([l2h_set[-1,:2]]) #value of low at hires heldout pt.
+		      for itr in range(ntr)]).mean()
+
+	mc1 = sample2(p2l_set[:,:-1], p2l_set[:,-1], hypers) 
+	tmp1 = np.array([mc1.GP.f.trace()[itr]([l2h_set[-1,:2]])  #value of low at hires heldout pt.
+		      for itr in range(ntr)]).mean()
+	
+	mc_l2h = sample2(l2h_set[:-1,-2], l2h_set[:-1,-1], hypers) #leave out last point; dont use prms
+	ntr = len(mc_l2h.GP.f.trace())
+	
+	est2 = np.array([mc_l2h.GP.f.trace()[itr]([tmp2]) #value at hires heldout pt. without extra point
+		      for itr in range(ntr)]).mean()
+
+	est1 = np.array([mc_l2h.GP.f.trace()[itr]([tmp1]) #value at hires heldout pt. with extra point
+		      for itr in range(ntr)]).mean()
+	# print tmp2,tmp1,l2h_set[-1,-2], est2,est1,l2h_set[-1,-1]
+	return abs(est2 - l2h_set[-1,-1]) - abs(est1 - l2h_set[-1,-1]) 
+
+
+def like_pair(input):
+
+	pair = input[0]
+	p2l_full = input[1][0]
+	l2h_full = input[1][1]
+
+	try:
+		hypers = input[1][2:]
+	except IndexError:
+		hypers = [None]*3
+		
+	p2l_set = p2l_full.copy()
+	p2l_set = np.delete(p2l_set, pair[1], axis=0) # delete heldout l2h point from p2l
+
+	#Make copy and move pair to end
+	l2h_set = l2h_full.copy()
+	l2h_set[[-1,pair[1]]] = l2h_set[[pair[1],-1]] #holdout
+	l2h_set[[-2,pair[0]]] = l2h_set[[pair[0],-2]] #test point
+
+	mc_p2l = sample2(p2l_set[:-1,:-1], p2l_set[:-1,-1], hypers) #leave out last point
+	ntr = len(mc_p2l.GP.f.trace())
+
+	tmp = np.array([mc_p2l.GP.f.trace()[itr]([l2h_set[-1,:2]]) #value of low at hires heldout pt.
+		      for itr in range(ntr)]).mean()
+
+	mc_l2h_1 = sample2(l2h_set[:-1,-2], l2h_set[:-1,-1], hypers) #Use testpt.; leave out heldout pt; dont use prms
+	ntr = len(mc_l2h_1.GP.f.trace())
+	
+	est1 = np.array([mc_l2h_1.GP.f.trace()[itr]([tmp]) #value at heldout pt. without test point
+		      for itr in range(ntr)]).mean()
+
+	mc_l2h_2 = sample2(l2h_set[:-2,-2], l2h_set[:-2,-1], hypers) #leave out testpt and heldout pt; dont use prms
+	est2 = np.array([mc_l2h_2.GP.f.trace()[itr]([tmp]) #value at heldout pt. without test point
+		      for itr in range(ntr)]).mean()
+
+	# print tmp2,tmp1,l2h_set[-1,-2], est2,est1,l2h_set[-1,-1]
+	return abs(est2 - l2h_set[-1,-1]) - abs(est1 - l2h_set[-1,-1]) 
